62ecd508fbf2df426c2a1c71770b335530039cec
Brett Nisbett <brett.nisbett@thescore.com>
2017-06-03 18:59:08 -0400

    add skeleton quiz and question models

---
+++ spec/factories/quizzes.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :quiz do
+    name "MyString"
+    title "MyString"
+    description "MyString"
+    start_at "2017-06-03 17:37:56"
+    end_at "2017-06-03 17:37:56"
+  end
+end
